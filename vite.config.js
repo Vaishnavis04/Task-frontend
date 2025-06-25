e2a74be7436fba911508fce9c9a10939e8a3d491
Vaishnavis04 <136159499+Vaishnavis04@users.noreply.github.com>
2025-06-25 12:37:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-    
-  ],
-      
+import reactPlugin from '@vitejs/plugin-react';
 
-})
+export default {
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+};
